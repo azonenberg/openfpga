8e30e020d9c0645bd564a89aa83ae431d8efc7d9
Andrew Zonenberg <azonenberg@drawersteak.com>
2016-07-11 22:46:13 -0700

    Added Dac test (not yet working)

---
+++ tests/greenpak4/Dac/Dac.v
@@ -0,0 +1,98 @@
+/***********************************************************************************************************************
+ * Copyright (C) 2016 Andrew Zonenberg and contributors                                                                *
+ *                                                                                                                     *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General   *
+ * Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) *
+ * any later version.                                                                                                  *
+ *                                                                                                                     *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied  *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for     *
+ * more details.                                                                                                       *
+ *                                                                                                                     *
+ * You should have received a copy of the GNU Lesser General Public License along with this program; if not, you may   *
+ * find one here:                                                                                                      *
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt                                                              *
+ * or you may search the http://www.gnu.org website for the version 2.1 license, or you may write to the Free Software *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA                                      *
+ **********************************************************************************************************************/
+
+`default_nettype none
+
+/**
+	OUTPUTS:
+		Bandgap OK on pin 20 (should be high after reset)
+		DAC output on pin 19 (sweeping TBD)
+		
+	TEST PROCEDURE:
+		TODO
+ */
+module Dac(bg_ok, vout);
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// I/O declarations
+	
+	(* LOC = "P20" *)
+	output wire bg_ok;
+	
+	(* LOC = "P19" *)
+	(* IBUF_TYPE = "ANALOG" *)
+	output wire vout;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// System reset stuff
+
+	//Power-on reset
+	wire por_done;
+	GP_POR #(
+		.POR_TIME(500)
+	) por (
+		.RST_DONE(por_done)
+	);
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Oscillators
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// 1.0V voltage reference for the DAC
+	
+	//Configure the bandgap with default timing etc
+	GP_BANDGAP #(
+		.AUTO_PWRDN(0),
+		.CHOPPER_EN(1),
+		.OUT_DELAY(550)
+	) bandgap (
+		.OK(bg_ok)
+	);
+	
+	//1.0V reference for the DAC
+	wire vref_1v0;
+	GP_VREF #(
+		.VIN_DIV(4'd1),
+		.VREF(16'd1000)
+	) vr1000 (
+		.VIN(1'b0),
+		.VOUT(vref_1v0)
+	);
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// DAC driving the voltage reference
+	
+	wire vdac;
+	GP_DAC dac(
+		.DIN(8'h80),
+		.VOUT(vdac),
+		.VREF(vref_1v0)
+	);
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Voltage reference to buffer the DAC output
+	
+	GP_VREF #(
+		.VIN_DIV(4'd1),
+		.VREF(16'd750)
+	) vr750 (
+		.VIN(vdac),
+		.VOUT(vout)
+	);
+	
+endmodule
