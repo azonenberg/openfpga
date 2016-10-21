893f3bbb15579efede9823cd4f7d31bc061d8d8f
Andrew Zonenberg <azonenberg@drawersteak.com>
2016-10-20 21:46:04 -0700

    tests: Initial skeleton of HIL_PGA test

---
+++ tests/greenpak4/HIL_PGA.cpp
@@ -0,0 +1,35 @@
+/***********************************************************************************************************************
+ * Copyright (C) 2016 Andrew Zonenberg and contributors                                                                *
+ *                                                                                                                     *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General   *
+ * Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) *
+ * any later version.                                                                                                  *
+ *                                                                                                                     *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied  *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for     *
+ * more details.                                                                                                       *
+ *                                                                                                                     *
+ * You should have received a copy of the GNU Lesser General Public License along with this program; if not, you may   *
+ * find one here:                                                                                                      *
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt                                                              *
+ * or you may search the http://www.gnu.org website for the version 2.1 license, or you may write to the Free Software *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA                                      *
+ **********************************************************************************************************************/
+
+#include <log.h>
+#include <gpdevboard.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	g_log_sinks.emplace(g_log_sinks.begin(), new STDLogSink(Severity::DEBUG));
+
+	//expect one arg: the bitstream
+	if(argc != 2)
+		LogFatal("Usage: [testcase] bitstream.txt");
+
+	//
+
+	return 0;
+}
