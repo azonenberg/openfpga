e849edc1d780dc729888f6e1177ba14354c12615
Andrew Zonenberg <azonenberg@drawersteak.com>
2016-10-23 22:35:29 -0700

    tests: Added loopback test bitstreams for all supported devices

---
+++ tests/greenpak4/SocketTestLoopback_STQFN20.v
@@ -0,0 +1,51 @@
+/***********************************************************************************************************************
+ * Copyright (C) 2016 Andrew Zonenberg and contributors                                                                *
+ *                                                                                                                     *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General   *
+ * Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) *
+ * any later version.                                                                                                  *
+ *                                                                                                                     *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied  *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for     *
+ * more details.                                                                                                       *
+ *                                                                                                                     *
+ * You should have received a copy of the GNU Lesser General Public License along with this program; if not, you may   *
+ * find one here:                                                                                                      *
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt                                                              *
+ * or you may search the http://www.gnu.org website for the version 2.1 license, or you may write to the Free Software *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA                                      *
+ **********************************************************************************************************************/
+
+`default_nettype none
+
+/**
+	INPUTS:
+		Test input on pin 2
+
+	OUTPUTS:
+		The same signal echoed to all other GPIOs
+
+	TEST PROCEDURE:
+		Send arbitrary digital waveform into pin 2
+		Expect the same waveform echoed from all other pins
+
+		Note that this will detect open circuit or stuck-at faults in the socket, but not shorts between pins.
+ */
+module SocketTestLoopback_STQFN20(din, dout);
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// I/O declarations
+
+	(* LOC = "P2" *)
+	input wire din;
+
+	(* LOC = "P20 P19 P18 P17 P16 P15 P14 P13 P12 P10 P9 P8 P7 P6 P5 P4 P3" *)
+	output wire[16:0] dout;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// The actual logic
+
+	//... if you can even call it that!
+	assign dout = {17{din}};
+
+endmodule
