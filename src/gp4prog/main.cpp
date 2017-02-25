a2300236dd7dec2f43b63b8714b0151938136da2
Robert Ou <rqou@robertou.com>
2017-02-24 23:06:19 -0800

    gp4prog: Include <cstdlib>
    Some functions used here (e.g. atoi) are supposed to live in <cstdlib>.
    Fixes build on macOS.

--- src/gp4prog/main.cpp
+++ src/gp4prog/main.cpp
@@ -16,6 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA                                      *
  **********************************************************************************************************************/
 
+#include <cstdlib>
 #include <cstring>
 #include <cmath>
 #include <unistd.h>
