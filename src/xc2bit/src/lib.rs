e24853360c7b932d7bbe1ba3d3ba66c2d1ce29a7
Robert Ou <rqou@robertou.com>
2017-06-10 04:39:42 -0700

    xc2bit: Add deliberately-incorrect 256-MC ZIA map

--- src/xc2bit/src/lib.rs
+++ src/xc2bit/src/lib.rs
@@ -75,7 +75,7 @@ mod pla;
 pub use pla::{XC2PLAAndTerm, XC2PLAOrTerm};
 
 mod zia;
-pub use zia::{XC2ZIARowPiece, XC2ZIAInput, ZIA_MAP_32, ZIA_MAP_64, ZIA_MAP_128};
+pub use zia::{XC2ZIARowPiece, XC2ZIAInput, ZIA_MAP_32, ZIA_MAP_64, ZIA_MAP_128, ZIA_MAP_256};
 
 mod jed;
 pub use jed::{read_jed};
