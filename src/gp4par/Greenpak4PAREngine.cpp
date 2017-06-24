b53f2e2e847574887033837c7678d217b7bc84bf
Andrew Zonenberg <azonenberg@drawersteak.com>
2017-06-24 14:58:44 -0700

    gp4par: Changed congestion metrics for better avoidance of congestion

--- src/gp4par/Greenpak4PAREngine.cpp
+++ src/gp4par/Greenpak4PAREngine.cpp
@@ -200,7 +200,7 @@ uint32_t Greenpak4PAREngine::ComputeCongestionCost() const
 
 	//Squaring each half makes minimizing the larger one more important
 	//vs if we just summed. Also add in a fixed penalty if we are using >100% resources
-	uint32_t cost = sqrt(costs[0]*costs[0] + costs[1]*costs[1]);
+	uint32_t cost = costs[0]*costs[0] + costs[1]*costs[1];
 	if( (costs[0] > 10) || (costs[1] > 10) )
 		cost += 20;
 	return cost;
