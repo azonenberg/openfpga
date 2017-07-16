479fe3dae59600704b3427ba8a9a456711195082
Robert Ou <rqou@robertou.com>
2017-07-16 15:29:01 -0700

    Change test

--- src/xc2par/test.v
+++ src/xc2par/test.v
@@ -1,9 +1,39 @@
-module top(a, b, o);
+module top(clk_, o);
 
 output wire o;
-input a;
-input b;
+input clk_;
 
-assign o = a & b;
+BUFG clk_bufg (
+    .I(clk_),
+    .O(clk));
 
+FTDCP toggle (
+    .C(clk),
+    .PRE(1'b0),
+    .CLR(1'b0),
+    .T(1'b1),
+    .Q(o));
 endmodule
+
+// module FTCP (C, PRE, CLR, T, Q);
+//     parameter INIT = 0;
+
+//     input C, PRE, CLR, T;
+//     output wire Q;
+//     reg Q_;
+
+//     initial begin
+//         Q_ <= INIT;
+//     end
+
+//     always @(posedge C, posedge PRE, posedge CLR) begin
+//         if (CLR == 1)
+//             Q_ <= 0;
+//         else if (PRE == 1)
+//             Q_ <= 1;
+//         else if (T == 1)
+//             Q_ <= ~Q_;
+//     end
+
+//     assign Q = Q_;
+// endmodule
