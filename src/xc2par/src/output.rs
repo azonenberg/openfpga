449ed8ddf9030d7b3fcc5bf3d48227e7a571a1c3
Robert Ou <rqou@robertou.com>
2017-11-02 23:59:31 -0700

    xc2par: Giant hack to successfully output AND terms

--- src/xc2par/src/output.rs
+++ src/xc2par/src/output.rs
@@ -29,7 +29,7 @@ use self::xc2bit::*;
 use *;
 use objpool::*;
 
-pub fn produce_bitstream(device_type: XC2Device, g: &NetlistGraph,
+pub fn produce_bitstream(device_type: XC2Device, g: &NetlistGraph, mcs: &[NetlistMacrocell],
     placements: &[([(isize, isize); MCS_PER_FB], [Option<ObjPoolIndex<NetlistGraphNode>>; ANDTERMS_PER_FB],
         [XC2ZIAInput; INPUTS_PER_ANDTERM])]) -> XC2Bitstream {
 
@@ -41,7 +41,213 @@ pub fn produce_bitstream(device_type: XC2Device, g: &NetlistGraph,
 
     let mut extra_inpin = XC2ExtraIBuf::default();
 
+    // ZIA settings
+    for fb_i in 0..placements.len() {
+        for zia_i in 0..INPUTS_PER_ANDTERM {
+            fb_bits[fb_i].zia_bits[zia_i] = XC2ZIARowPiece{selected: placements[fb_i].2[zia_i]};
+        }
+    }
 
+    // AND terms
+    for fb_i in 0..placements.len() {
+        for andterm_i in 0..ANDTERMS_PER_FB {
+            let andterm_idx = placements[fb_i].1[andterm_i];
+            if andterm_idx.is_some() {
+                let andterm_idx = andterm_idx.unwrap();
+                let andterm_node = g.nodes.get(andterm_idx);
+
+                // FIXME: Code duplication
+                if let NetlistGraphNodeVariant::AndTerm{ref inputs_true, ref inputs_comp, ..} = andterm_node.variant {
+                    // Collect the inputs that need to go into this FB
+                    let mut collected_inputs_true_vec = Vec::new();
+                    let mut collected_inputs_comp_vec = Vec::new();
+
+                    for &input_net in inputs_true {
+                        let input_node = g.nodes.get(g.nets.get(input_net).source.unwrap().0);
+                        if let NetlistGraphNodeVariant::ZIADummyBuf{input, ..} = input_node.variant {
+                            let input_real_node_idx = g.nets.get(input).source.unwrap().0;
+                            collected_inputs_true_vec.push(input_real_node_idx);
+                        } else {
+                            panic!("not a zia buf");
+                        }
+                    }
+                    for &input_net in inputs_comp {
+                        let input_node = g.nodes.get(g.nets.get(input_net).source.unwrap().0);
+                        if let NetlistGraphNodeVariant::ZIADummyBuf{input, ..} = input_node.variant {
+                            let input_real_node_idx = g.nets.get(input).source.unwrap().0;
+                            collected_inputs_comp_vec.push(input_real_node_idx);
+                        } else {
+                            panic!("not a zia buf");
+                        }
+                    }
+
+                    // Find correct setting
+                    let zia_true_settings = collected_inputs_true_vec.iter().map(|input| {
+                        // XXX FIXME
+                        let mut fbmc = None;
+                        for fb_i in 0..placements.len() {
+                            for mc_i in 0..MCS_PER_FB {
+                                if placements[fb_i].0[mc_i].0 >= 0 {
+                                    let netlist_mc = &mcs[placements[fb_i].0[mc_i].0 as usize];
+                                    let (i, need_to_use_ibuf_zia_path) = match netlist_mc {
+                                        &NetlistMacrocell::PinOutput{i} => (i, false),
+                                        &NetlistMacrocell::BuriedComb{i} => (i, false),
+                                        &NetlistMacrocell::BuriedReg{i, has_comb_fb} => (i, has_comb_fb),
+                                        _ => unreachable!(),
+                                    };
+                                    if i == (*input) {
+                                        fbmc = Some((fb_i, mc_i, need_to_use_ibuf_zia_path));
+                                        break;
+                                    }
+                                }
+                                if placements[fb_i].0[mc_i].1 >= 0 {
+                                    let netlist_mc = &mcs[placements[fb_i].0[mc_i].1 as usize];
+                                    let i = match netlist_mc {
+                                        &NetlistMacrocell::PinInputUnreg{i} => i,
+                                        &NetlistMacrocell::PinInputReg{i} => i,
+                                        _ => unreachable!(),
+                                    };
+                                    if i == (*input) {
+                                        fbmc = Some((fb_i, mc_i, false));
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        let (fb, mc, need_to_use_ibuf_zia_path) = fbmc.expect("macrocell not assigned");
+
+                        // What input do we actually want?
+                        let input_obj = g.nodes.get(*input);
+                        let choice = match input_obj.variant {
+                            NetlistGraphNodeVariant::InBuf{..} => {
+                                // FIXME: Hack
+                                if true && fb == 2 && mc == 0 {
+                                    XC2ZIAInput::DedicatedInput
+                                } else {
+                                    XC2ZIAInput::IBuf{ibuf: fb_mc_num_to_iob_num(
+                                        XC2Device::XC2C32A, fb as u32, mc as u32).unwrap()}
+                                }
+                            },
+                            NetlistGraphNodeVariant::IOBuf{..} => {
+                                XC2ZIAInput::IBuf{ibuf: fb_mc_num_to_iob_num(
+                                    XC2Device::XC2C32A, fb as u32, mc as u32).unwrap()}
+                            },
+                            NetlistGraphNodeVariant::Xor{..} => {
+                                XC2ZIAInput::Macrocell{fb: fb as u32, mc: mc as u32}
+                            },
+                            NetlistGraphNodeVariant::Reg{..} => {
+                                if need_to_use_ibuf_zia_path {
+                                    XC2ZIAInput::IBuf{ibuf: fb_mc_num_to_iob_num(
+                                        XC2Device::XC2C32A, fb as u32, mc as u32).unwrap()}
+                                } else {
+                                    XC2ZIAInput::Macrocell{fb: fb as u32, mc: mc as u32}
+                                }
+                            }
+                            _ => panic!("not a valid input"),
+                        };
+
+                        choice
+                    }).collect::<Vec<_>>();
+                    let zia_comp_settings = collected_inputs_comp_vec.iter().map(|input| {
+                        // XXX FIXME
+                        let mut fbmc = None;
+                        for fb_i in 0..placements.len() {
+                            for mc_i in 0..MCS_PER_FB {
+                                if placements[fb_i].0[mc_i].0 >= 0 {
+                                    let netlist_mc = &mcs[placements[fb_i].0[mc_i].0 as usize];
+                                    let (i, need_to_use_ibuf_zia_path) = match netlist_mc {
+                                        &NetlistMacrocell::PinOutput{i} => (i, false),
+                                        &NetlistMacrocell::BuriedComb{i} => (i, false),
+                                        &NetlistMacrocell::BuriedReg{i, has_comb_fb} => (i, has_comb_fb),
+                                        _ => unreachable!(),
+                                    };
+                                    if i == (*input) {
+                                        fbmc = Some((fb_i, mc_i, need_to_use_ibuf_zia_path));
+                                        break;
+                                    }
+                                }
+                                if placements[fb_i].0[mc_i].1 >= 0 {
+                                    let netlist_mc = &mcs[placements[fb_i].0[mc_i].1 as usize];
+                                    let i = match netlist_mc {
+                                        &NetlistMacrocell::PinInputUnreg{i} => i,
+                                        &NetlistMacrocell::PinInputReg{i} => i,
+                                        _ => unreachable!(),
+                                    };
+                                    if i == (*input) {
+                                        fbmc = Some((fb_i, mc_i, false));
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        let (fb, mc, need_to_use_ibuf_zia_path) = fbmc.expect("macrocell not assigned");
+
+                        // What input do we actually want?
+                        let input_obj = g.nodes.get(*input);
+                        let choice = match input_obj.variant {
+                            NetlistGraphNodeVariant::InBuf{..} => {
+                                // FIXME: Hack
+                                if true && fb == 2 && mc == 0 {
+                                    XC2ZIAInput::DedicatedInput
+                                } else {
+                                    XC2ZIAInput::IBuf{ibuf: fb_mc_num_to_iob_num(
+                                        XC2Device::XC2C32A, fb as u32, mc as u32).unwrap()}
+                                }
+                            },
+                            NetlistGraphNodeVariant::IOBuf{..} => {
+                                XC2ZIAInput::IBuf{ibuf: fb_mc_num_to_iob_num(
+                                    XC2Device::XC2C32A, fb as u32, mc as u32).unwrap()}
+                            },
+                            NetlistGraphNodeVariant::Xor{..} => {
+                                XC2ZIAInput::Macrocell{fb: fb as u32, mc: mc as u32}
+                            },
+                            NetlistGraphNodeVariant::Reg{..} => {
+                                if need_to_use_ibuf_zia_path {
+                                    XC2ZIAInput::IBuf{ibuf: fb_mc_num_to_iob_num(
+                                        XC2Device::XC2C32A, fb as u32, mc as u32).unwrap()}
+                                } else {
+                                    XC2ZIAInput::Macrocell{fb: fb as u32, mc: mc as u32}
+                                }
+                            }
+                            _ => panic!("not a valid input"),
+                        };
+
+                        choice
+                    }).collect::<Vec<_>>();
+
+                    let zia_true_settings_idx = zia_true_settings.into_iter().map(|x| {
+                        let mut i = None;
+                        for zia_i in 0..INPUTS_PER_ANDTERM {
+                            if placements[fb_i].2[zia_i] == x {
+                                i = Some(zia_i);
+                                break;
+                            }
+                        }
+                        i.unwrap()
+                    }).collect::<Vec<_>>();
+                    let zia_comp_settings_idx = zia_comp_settings.into_iter().map(|x| {
+                        let mut i = None;
+                        for zia_i in 0..INPUTS_PER_ANDTERM {
+                            if placements[fb_i].2[zia_i] == x {
+                                i = Some(zia_i);
+                                break;
+                            }
+                        }
+                        i.unwrap()
+                    }).collect::<Vec<_>>();
+
+                    for x in zia_true_settings_idx {
+                        fb_bits[fb_i].and_terms[andterm_i].input[x] = true;
+                    }
+                    for x in zia_comp_settings_idx {
+                        fb_bits[fb_i].and_terms[andterm_i].input_b[x] = true;
+                    }
+                } else {
+                    panic!("not an and");
+                }
+            }
+        }
+    }
 
     XC2Bitstream {
         speed_grade: XC2Speed::Speed6,
