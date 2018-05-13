5540ca03a51bed1c58e76e3ad350caae47b0e834
Robert Ou <rqou@robertou.com>
2018-05-13 15:54:34 -0700

    xc2par: Hack build.rs so that it uploads

--- src/xc2par/build.rs
+++ src/xc2par/build.rs
@@ -7,28 +7,30 @@ fn one_set_of_reftests(outfile: &'static str, indir: &'static str) {
 
     let root_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let reftests_dir = std::path::Path::new(&root_dir).join(indir);
-    let files = std::fs::read_dir(reftests_dir).unwrap();
+    let files = std::fs::read_dir(reftests_dir);
 
-    for file in files {
-        let path = file.expect("failed to get path").path();
-        let ext = path.extension().expect("bogus reftest filename (doesn't have extension)");
-        if ext == "json" || ext == "fail" {
-            let path = path.canonicalize().unwrap();
+    if let Ok(files) = files {
+        for file in files {
+            let path = file.expect("failed to get path").path();
+            let ext = path.extension().expect("bogus reftest filename (doesn't have extension)");
+            if ext == "json" || ext == "fail" {
+                let path = path.canonicalize().unwrap();
 
-            let id_string = path.file_name().unwrap().to_str().unwrap().chars().map(|x| match x {
-                'A'...'Z' | 'a'...'z' | '0'...'9' => x,
-                _ => '_',
-            }).collect::<String>();
+                let id_string = path.file_name().unwrap().to_str().unwrap().chars().map(|x| match x {
+                    'A'...'Z' | 'a'...'z' | '0'...'9' => x,
+                    _ => '_',
+                }).collect::<String>();
 
-            if ext == "fail" {
-                write!(f, "#[should_panic]\n").unwrap();
+                if ext == "fail" {
+                    write!(f, "#[should_panic]\n").unwrap();
+                }
+                write!(f, r#"
+                    #[test]
+                    fn reftest_{}() {{
+                        run_one_reftest("{}");
+                    }}
+                    "#, id_string, path.to_str().unwrap()).unwrap();
             }
-            write!(f, r#"
-                #[test]
-                fn reftest_{}() {{
-                    run_one_reftest("{}");
-                }}
-                "#, id_string, path.to_str().unwrap()).unwrap();
         }
     }
 }
