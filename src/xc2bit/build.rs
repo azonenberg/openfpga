943ea5e6812a526e5d252bf869acbef3791d120b
Robert Ou <rqou@robertou.com>
2018-05-13 15:49:45 -0700

    xc2bit: Hack build.rs so that it uploads

--- src/xc2bit/build.rs
+++ src/xc2bit/build.rs
@@ -7,24 +7,26 @@ fn main() {
 
     let root_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let reftests_dir = std::path::Path::new(&root_dir).join("../../tests/xc2bit/reftests");
-    let files = std::fs::read_dir(reftests_dir).unwrap();
+    let files = std::fs::read_dir(reftests_dir);
 
-    for file in files {
-        let path = file.expect("failed to get path").path();
-        if path.extension().expect("bogus reftest filename (doesn't have extension)") == "jed" {
-            let path = path.canonicalize().unwrap();
+    if let Ok(files) = files {
+        for file in files {
+            let path = file.expect("failed to get path").path();
+            if path.extension().expect("bogus reftest filename (doesn't have extension)") == "jed" {
+                let path = path.canonicalize().unwrap();
 
-            let id_string = path.file_name().unwrap().to_str().unwrap().chars().map(|x| match x {
-                'A'...'Z' | 'a'...'z' | '0'...'9' => x,
-                _ => '_',
-            }).collect::<String>();
+                let id_string = path.file_name().unwrap().to_str().unwrap().chars().map(|x| match x {
+                    'A'...'Z' | 'a'...'z' | '0'...'9' => x,
+                    _ => '_',
+                }).collect::<String>();
 
-            write!(f, r#"
-                #[test]
-                fn reftest_{}() {{
-                    run_one_reftest("{}");
-                }}
-                "#, id_string, path.to_str().unwrap()).unwrap();
+                write!(f, r#"
+                    #[test]
+                    fn reftest_{}() {{
+                        run_one_reftest("{}");
+                    }}
+                    "#, id_string, path.to_str().unwrap()).unwrap();
+            }
         }
     }
 }
